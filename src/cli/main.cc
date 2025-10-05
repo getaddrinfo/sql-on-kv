e211081e707504c3e16652de6890f6c773e0a081
Personal <oss@getaddrinfo.com>
2025-10-05 21:38:16 +0100

    chore(cli): update for testing

--- src/cli/main.cc
+++ src/cli/main.cc
@@ -1,12 +1,14 @@
 #include <string>
 
 #include "sql/lexer.hh"
+#include "sql/parser.hh"
+
 #include "absl/log/log.h"
 
 int main(void) {
-  const std::string code = "SELECT * FROM users WHERE id IN (1, 2, 3);";
+  const std::string code = "SELECT id, name FROM users WHERE ((id = 1 AND id=2) OR (id=3));";
 
   std::vector<sql::lexer::Token> tokens = sql::lexer::lex(code);
-
-  LOG(INFO) << tokens.size();
+  sql::parser::ParseResult result = sql::parser::parse(tokens);
+  (void)result;
 }
