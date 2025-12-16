8393b6299ca8a2e2a492bc24cc0deff53fe891eb
Personal <oss@getaddrinfo.com>
2025-12-16 19:01:01 +0000

    feat(lib): bitflags

---
+++ src/lib/flags.hh
@@ -0,0 +1,53 @@
+#ifndef _LIB_FLAGS_HH
+#define _LIB_FLAGS_HH
+
+#include <type_traits>
+
+namespace lib {
+  template <
+    typename T, 
+    std::enable_if_t<
+      std::conjunction_v<
+        std::is_enum<T>,
+        std::is_integral<std::underlying_type_t<T>>
+      >
+    > = 0
+  >
+  class flags {
+    using BitType = std::underlying_type_t<T>;
+    BitType _value;
+
+  public:
+    flags(BitType value) : _value(value) {}
+
+    const bool has(T flag) const {
+      BitType mask = static_cast<BitType>(flag);
+
+      return (_value & mask) == mask;
+    }
+
+    void set(T flag) {
+      BitType mask = static_cast<BitType>(flag);
+      
+      _value |= mask;
+    }
+    void unset(T flag) {
+      BitType mask = static_cast<BitType>(flag);
+
+      _value &= (~mask);
+    }
+
+    void toggle(T flag) {
+      BitType mask = static_cast<BitType>(flag);
+
+      _value ^= mask;
+    }
+  };
+
+  template <typename T>
+  class flags {
+    static_assert(!std::is_same_v<T, T>, "Underlying type must be an enum with a fixed width")
+  };
+}
+
+#endif /* _LIB_FLAGS_HH */
